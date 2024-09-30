861d57d3350f37507ddc8d95c07891e45d84674d
Amit <amitkuswain6@gmail.com>
2024-09-30 14:44:23 +0530

    for Deleting from arrays

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
-export default function TodoList(){
-    let[todos, setTodos]=useState([{task:"sample-task", id:uuidv4()}]);
-    let[newTodo,setNewTodo]=useState("");
+export default function TodoList() {
+    let [todos, setTodos] = useState([{ task: "sample-task", id: uuidv4() }]);
+    let [newTodo, setNewTodo] = useState("");
 
-    let addNewTask =()=>{
-       setTodos([...todos,{task:newTodo,id:uuidv4()}]);
-       setNewTodo("");
+    let addNewTask = () => {
+        setTodos((prevTodos) => {
+            return [...todos, { task: newTodo, id: uuidv4() }];
+        });
+        setNewTodo("");
     }
-    let UpdateTask=(event)=>{
-       setNewTodo(event.target.value);
+    let UpdateTask = (event) => {
+        setNewTodo(event.target.value);
+    }
+    let deleteTodo = (id) =>{
+    setTodos( (prevTodos)=>todos.filter((prevTodos)=>  prevTodos.id != id));
+     
     }
 
     return (
         <div>
-            <input placeholder="add a task" value={newTodo}  onChange={UpdateTask}></input>
+            <input placeholder="add a task" value={newTodo} onChange={UpdateTask}></input>
             <br />
             <button onClick={addNewTask}> Add task</button>
             <br />
@@ -22,14 +28,18 @@ export default function TodoList(){
             <br />
 
             <hr />
-        <h4>Todo List</h4>
-        <ul>
-            {
-                todos.map((todo)=>(
-                   <li key={todo.id}>{todo.task}</li> 
-                ))
-            }
-        </ul>
+            <h4>Todo List</h4>
+            <ul>
+                {
+                    todos.map((todo) => (
+                        <li key={todo.id}>
+                         <span>  {todo.task} </span>
+                         &nbsp;&nbsp;&nbsp;
+                         <button onClick={()=>deleteTodo(todo.id)}>delete</button>
+                            </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
